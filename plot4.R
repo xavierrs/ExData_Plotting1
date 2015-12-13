6dd37c45e76ebeeb9abc8a53781f57b95c32cde5
Xavier Rodríguez <xavier.rs@fastmail.com>
2015-12-14 00:50:55 +0100

    Fixed plot4

--- plot4.R
+++ plot4.R
@@ -12,25 +12,24 @@ mydata$Date <- strptime(paste(mydata$Date, mydata$Time), "%d/%m/%Y %H:%M:%S")
 png("plot4.png", width = 480, height = 480)
 
 # Plot
-with(mydata, {
-    # Subplot 1
-    plot(Date, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+par(mfrow = c(2,2))
+# Subplot 1
+plot(mydata$Date, mydata$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
 
-    # Subplot 2
-    plot(Date, Voltage, type = "l", ylab = "Global Active Power")
-    
-    # Subplot 3
-    plot(Date, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
-    
-    lines(Date, Sub_metering_2, type = "l", col = "red")
-    lines(Date, Sub_metering_3, type = "l", col = "blue")
-    
-    # Subplot 3
-    legend("topright", lty = c(1, 1, 1), bty = "n", col = c("black", "red", "blue"),
-           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-    
-    plot(Date, Global_reactive_power, type = "l")
-})
+# Subplot 2
+plot(mydata$Date, mydata$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+
+# Subplot 3
+plot(mydata$Date, mydata$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+
+lines(mydata$Date, mydata$Sub_metering_2, type = "l", col = "red")
+lines(mydata$Date, mydata$Sub_metering_3, type = "l", col = "blue")
+
+legend("topright", lty = c(1, 1, 1), bty = "n", col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# Subplot 4
+plot(mydata$Date, mydata$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 
 # Save plot to file
 dev.off()
